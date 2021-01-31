c56d4c3edaf7043c9dd64cc08496b74995c7320b
oeccsy <oeccsy@users.noreply.github.com>
2021-01-31 20:14:38 +0900

    Bronze5 (210131)
    https://www.acmicpc.net/problem/2475
    https://www.acmicpc.net/problem/2557
    https://www.acmicpc.net/problem/2558
    https://www.acmicpc.net/problem/2845
    https://www.acmicpc.net/problem/2914
    https://www.acmicpc.net/problem/3003
    https://www.acmicpc.net/problem/3046
    
    백준 2475번 문제, Bronze5
    백준 2557번 문제, Bronze5
    백준 2558번 문제, Bronze5
    백준 2845번 문제, Bronze5
    백준 2914번 문제, Bronze5
    백준 3003번 문제, Bronze5
    백준 3046번 문제, Bronze5
    
    간단한 Bronze5 단계 문제들로 C#의 기초를 다졌다.
    
    21.01.31

---
+++ C#/No_2845.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace D_210131
+{
+    class No_2845
+    {
+        static void Main(string[] args)
+        {
+            string[] input_LP = Console.ReadLine().Split(' ');
+            string[] input_num = Console.ReadLine().Split(' ');
+
+            int LP = Convert.ToInt32(input_LP[0])*Convert.ToInt32(input_LP[1]);
+            
+            int[] num = new int[5];
+
+            for(int i=0; i<num.Length; i++)
+            {
+                num[i] = Convert.ToInt32(input_num[i]);
+                num[i] = num[i] - LP;
+
+                Console.Write($"{num[i]} ");
+            }
+
+        }
+    }
+}
---
+++ C#/No_3003.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace D_210131
+{
+    class No_3003
+    {
+        static void Main(string[] args)
+        {
+            string input_num = Console.ReadLine();
+            string[] str_num = input_num.Split(' ');
+
+            int[] num = new int[6];
+            int[] whitePiece = { 1, 1, 2, 2, 2, 8 };
+
+            for(int i=0; i<num.Length; i++)
+            {
+                num[i] = Convert.ToInt32(str_num[i]);
+                whitePiece[i] -= num[i];
+
+                Console.Write("{0} ", whitePiece[i]);
+            }
+        }
+    }
+}
