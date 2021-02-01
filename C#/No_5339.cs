b757e0380c5378d76b476a6eb455657bc04c3ea1
oeccsy <oeccsy@users.noreply.github.com>
2021-02-01 20:46:01 +0900

    Bronze5 (210201)
    https://www.acmicpc.net/problem/5339
    https://www.acmicpc.net/problem/5522
    https://www.acmicpc.net/problem/5554
    https://www.acmicpc.net/problem/6749
    https://www.acmicpc.net/problem/7287
    https://www.acmicpc.net/problem/8370
    
    백준 5339번 문제, Bronze5
    백준 5522번 문제, Bronze5
    백준 5554번 문제, Bronze5
    백준 6749번 문제, Bronze5
    백준 7287번 문제, Bronze5
    백준 8370번 문제, Bronze5
    
    간단한 다국어문제와 \출력 방법에 대한 문제를 해결하였다.
    
    21.02.01

---
+++ C#/No_5339.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace D_210201
+{
+    class No_5339
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     /~\\");
+            Console.WriteLine("    ( oo|");
+            Console.WriteLine("    _\\=/_");
+            Console.WriteLine("   /  _  \\");
+            Console.WriteLine("  //|/.\\|\\\\");
+            Console.WriteLine(" ||  \\ /  ||");
+            Console.WriteLine("============");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+            Console.WriteLine("|          |");
+
+        }
+    }
+}
---
+++ C#/No_5554.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace D_210201
+{
+    class No_5554
+    {
+        static void Main(string[] args)
+        {
+            int minute = 0;
+            int second = 0;
+
+            for (int i = 0; i<4; i++)
+            {
+                second += Convert.ToInt32(Console.ReadLine());
+                
+                while(second >=60)
+                {
+                    minute += 1;
+                    second -= 60;
+                }
+            }
+
+            Console.WriteLine(minute);
+            Console.WriteLine(second);
+
+        }
+    }
+}
