146f22c6c3795b4636300a79af62513394e73f59
oeccsy <oeccsy@users.noreply.github.com>
2021-02-08 20:42:09 +0900

    Bronze4 (210208)
    https://www.acmicpc.net/problem/1297
    https://www.acmicpc.net/problem/1330
    https://www.acmicpc.net/problem/2420
    
    백준 1297번, 1330번, 2420번 문제, Bronze4
    
    자료형을 변환하는 간단한 문제를 해결하였다.
    
    21.02.08

---
+++ C#/No_1297.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace D_210208
+{
+    class No_1297
+    {
+        static void Main(string[] args)
+        {
+            double l; //대각선의 길이
+            double a, b, k; //너비와 높이의 비율을 ak:bk 로 한다.
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            l = Convert.ToDouble(input[0]);
+            a = Convert.ToDouble(input[1]);
+            b = Convert.ToDouble(input[2]);
+
+            k = Math.Sqrt(l * l / (a * a + b * b));
+
+            a *= k;
+            b *= k;
+
+            Console.WriteLine($"{(int)a} {(int)b}");
+
+        }
+    }
+}
