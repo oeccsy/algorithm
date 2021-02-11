b829572ca3c4ddf8b5a2c4e3e07951092ef57c95
oeccsy <oeccsy@users.noreply.github.com>
2021-02-11 22:53:10 +0900

    No_1009 (210211)
    https://www.acmicpc.net/problem/1009
    백준 1009번 문제, Bronze3
    
    시간초과로 인하여 문제 해결을 오랬동안 하지 못하였다. 다음부터는 시간을 줄이는 방법도 고안해야겠다.
    
    21.02.11

---
+++ C#/No_1009.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace D_210211
+{
+    class No_1009
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            int a = 0, b = 0;
+            int result = 0;
+
+            for(int i = 0; i<T; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                a = Convert.ToInt32(input[0]);
+                b = Convert.ToInt32(input[1]);
+                b = b % 4 + 4;
+
+                result = a;
+
+                for(int j = 1; j<b;j++)
+                {
+                    result *= a;
+                    result = result%10;
+                    
+
+                }
+
+                if (result == 0)
+                    result = 10;
+              
+
+                Console.WriteLine(result);
+
+
+            }
+
+        }
+    }
+}
