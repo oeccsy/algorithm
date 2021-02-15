998cf22ae89fd5508c6e53fd8b82800aa4220c3a
oeccsy <oeccsy@users.noreply.github.com>
2021-02-15 22:40:11 +0900

    No_2442 (210215)
    https://www.acmicpc.net/problem/2442
    백준 2442번 문제, Bronze3
    
    더 업그레이드 된 별찍기 문제에 도전하였다.
    
    21.02.15

---
+++ C#/No_2442.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace D_210215
+{
+    class No_2442
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i<input; i++)
+            {
+                for(int j = 0; j<input-i-1; j++)
+                {
+                    Console.Write(' ');
+                }
+
+                for(int j = 0; j<2*i+1; j++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
