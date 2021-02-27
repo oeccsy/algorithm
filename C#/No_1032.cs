b8b5db4b9ac8564953f228c3c7cc8f34ba0b5237
oeccsy <oeccsy@users.noreply.github.com>
2021-02-27 23:47:45 +0900

    No_1032 (210227)
    https://www.acmicpc.net/problem/1032
    백준 1032번 문제, Bronze1
    
    21.02.27

---
+++ C#/No_1032.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace D_210227
+{
+    class No_1032
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[] input = new string[n];
+            char[] result;
+
+            for(int i = 0; i<n; i++)
+            {
+                input[i] = Console.ReadLine();
+            }
+
+            result = new char[input[0].Length];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if (i == 0)
+                        result[j] = input[i][j];
+                    else if (result[j] != input[i][j])
+                        input[0][j] = '?';
+                }
+            }
+
+            for(int i =0; i<result.Length; i++)
+            {
+                Console.Write(result[i]);
+            }
+
+
+        }
+    }
+}
