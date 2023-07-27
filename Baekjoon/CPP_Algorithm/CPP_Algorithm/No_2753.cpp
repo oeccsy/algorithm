63c7be8e5f824b0e1e3f82a1a49cb2812dcccba0
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-07-27 16:59:50 +0900

    No_2753

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_2753.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int year;
+    bool is_leap;
+    cin >> year;
+
+    if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    {
+        is_leap = true;
+    }
+    else
+    {
+        is_leap = false;
+    }
+
+    cout << is_leap;
+    
+}
