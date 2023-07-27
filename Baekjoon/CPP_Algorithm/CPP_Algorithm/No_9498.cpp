5d277cfe54511f3041bf25e5045efd5787a2d441
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-07-27 16:52:21 +0900

    No_9498

--- Baekjoon/CPP_Algorithm/CPP_Algorithm/No_9498.cpp
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_9498.cpp
@@ -10,6 +10,25 @@ int main()
     int score;
     cin >> score;
     
-    if
-    cout << 
+    if(score >= 90)
+    {
+        cout << 'A';
+    }
+    else if (score >= 80)
+    {
+        cout << 'B';
+    }
+    else if (score >= 70)
+    {
+        cout << 'C';
+    }
+    else if (score >= 60)
+    {
+        cout << 'D';
+    }
+    else
+    {
+        cout << 'F';
+    }
+     
 }
