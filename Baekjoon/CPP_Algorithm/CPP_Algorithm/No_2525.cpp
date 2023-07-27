8a5e4e2a7601db35defaade10daff77c0ce8a882
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-07-27 17:18:04 +0900

    No_2525

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_2525.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a;
+    int b;
+    int c;
+
+    int end_h = 0;
+    int end_m = 0;
+    
+    cin >> a >> b;
+    cin >> c;
+
+    end_h = a;
+    end_m = b + c;
+
+    while(end_m >= 60)
+    {
+        end_h += 1;
+        end_m -= 60;
+    }
+
+    while(end_h >= 24)
+    {
+        end_h -= 24;
+    }
+
+    cout << end_h << " " << end_m;
+}
