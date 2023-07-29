1184f480e2e8a5aa5b63b84788cd0c36327e45f9
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-07-29 21:10:17 +0900

    No_10951.cpp

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_10951.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a,b;
+
+    while(true)
+    {
+        cin >> a >> b;
+
+        if(cin.eof())
+            break;
+        
+        cout << a + b << '\n';
+    }
+}
