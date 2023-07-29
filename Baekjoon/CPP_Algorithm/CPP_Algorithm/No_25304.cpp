3c30d0c5f0833c8c83359c8313a439deae0d33e6
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-07-29 20:29:00 +0900

    No_25304.cpp

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_25304.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long x;
+    int n;
+    int a,b;
+    
+    cin >> x;
+    cin >> n;
+    
+    for(int i=0; i<n; i++)
+    {
+        cin >> a >> b;
+        x -= a * b;
+    }
+
+    if(x == 0)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+
+    
+}
