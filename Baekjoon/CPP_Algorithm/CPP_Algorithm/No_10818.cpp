4a74692e32c667f0e9a3480bd06a1ad565162b1e
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-07-29 21:54:45 +0900

    No_10818.cpp

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_10818.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    array<int, 1000000> arr = {0, };
+
+    int min, max;
+
+    cin >> n;
+
+    for(int  i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    min = arr[0];
+    max = arr[0];
+
+    for(int i=1; i<n; i++)
+    {
+        if(arr[i] > max) max = arr[i];
+        if(arr[i] < min) min = arr[i];
+    }
+
+    cout << min << " " << max;
+    
+}
