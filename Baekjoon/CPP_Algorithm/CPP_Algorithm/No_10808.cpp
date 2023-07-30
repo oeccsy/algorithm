ecb14eb93e759272d20bbc0dd7570d096813554b
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-07-30 15:34:43 +0900

    No_10808

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_10808.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    array<int, 26> alphabet = {0, };
+    string input;
+
+    cin >> input;
+
+    for(int i=0; i<input.length(); i++)
+    {
+        alphabet[input[i] - 'a']++;
+    }
+
+    for(int i=0; i<alphabet.size(); i++)
+    {
+        cout << alphabet[i] << " ";   
+    }
+}
