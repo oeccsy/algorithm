35808bac7607b6798fb253ab2473d0cb9a994910
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-07-30 14:48:45 +0900

    No_10810

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_10810.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n,m;
+    int i,j,k;
+    array<int, 101> arr = {0, };
+
+    cin >> n >> m;
+
+    for(int iter=0; iter<m; iter++)
+    {
+        cin >> i >> j >> k;
+
+        for(int target=i; target<=j; target++)
+        {
+            arr[target] = k;
+        }
+    }
+
+    for(int iter = 1; iter<=n; iter++)
+    {
+        cout << arr[iter] << " ";
+    }
+}
