97ff7521cead536c16e691dc60521bb9d1a2bd8d
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-08-02 14:56:15 +0900

    No_10828

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_10828.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int stack[10001] = {0, };
+int pos = 0;
+
+void push(int x)
+{
+    stack[pos] = x;
+    pos++;
+}
+
+void pop()
+{
+    if(pos == 0)
+    {
+        cout << -1 << '\n';
+    }
+    else
+    {
+        pos--;
+        cout << stack[pos] << '\n';
+    }
+}
+
+void size()
+{
+    cout << pos << '\n';
+}
+
+void empty()
+{
+    if(pos == 0)
+    {
+        cout << 1 << '\n';
+    }
+    else
+    {
+        cout << 0 << '\n';
+    }
+}
+
+void top()
+{
+    if(pos == 0)
+    {
+        cout << -1 << '\n';
+    }
+    else
+    {
+        cout << stack[pos-1] << '\n';    
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for(int i=0; i<n; i++)
+    {
+        string input;
+        int num;
+        cin >> input;
+
+        if(input == "push")
+        {
+            cin >> num;
+            push(num);
+        }
+        if(input == "pop")
+        {
+            pop();
+        }
+        if(input == "size")
+        {
+            size();
+        }
+        if(input == "empty")
+        {
+            empty();
+        }
+        if(input == "top")
+        {
+            top();
+        }
+    }
+}
