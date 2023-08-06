6de005b1ecbacfba1247c38869c1f2a27e147a0b
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-08-06 10:52:27 +0900

    No_2164, No_10866

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_2164.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    queue<int> cards;
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++)
+    {
+        cards.push(i);
+    }
+
+    while(cards.size() > 1)
+    {
+        cards.pop();
+
+        cards.push(cards.front());
+        cards.pop();
+    }
+
+    cout << cards.front();
+}
