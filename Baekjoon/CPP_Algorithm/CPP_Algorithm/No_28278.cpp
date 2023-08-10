870c6937ebd75bb23473527a3ce94f5812cb3c64
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-08-10 21:01:14 +0900

    No_28278

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_28278.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    stack<int> stack;
+
+    for(int i=0; i<n; i++)
+    {
+        int input;
+        cin >> input;
+
+        if(input == 1)
+        {
+            int x;
+            cin >> x;
+
+            stack.push(x);
+        }
+        else if(input == 2)
+        {
+            if(stack.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << stack.top() << '\n';
+                stack.pop();
+            }
+        }
+        else if (input == 3)
+        {
+            cout << stack.size() << '\n';
+        }
+        else if (input == 4)
+        {
+            if(stack.empty())
+            {
+                cout << 1 << '\n';
+            }
+            else
+            {
+                cout << 0 << '\n';
+            }
+        }
+        else if (input == 5)
+        {
+            if(stack.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << stack.top() << '\n';
+            }
+        }
+    }
+}
