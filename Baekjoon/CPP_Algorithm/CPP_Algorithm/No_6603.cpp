1406e72b4623a85becccdbedcf0e26b85ab8be78
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-08-18 03:09:44 +0900

    No_6603

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_6603.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int k;
+int set[49];
+bool is_used[49];
+int selected[6];
+
+void Select(int index)
+{
+    if(index == 6)
+    {
+        for(int i=0; i<6; i++)
+        {
+            cout << selected[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=0; i<k; i++)
+    {
+        if(is_used[i]) continue;
+        if(index > 0 && selected[index-1] > set[i]) continue;
+        
+        selected[index] = set[i];
+        is_used[i] = true;
+        Select(index+1);
+        is_used[i] = false;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while(true)
+    {
+        cin >> k;
+        if(k == 0) break;
+
+        for(int i=0; i<k; i++)
+        {
+            int input;
+            cin >> input;
+
+            set[i] = input;
+        }
+
+        Select(0);
+        
+        cout << '\n';
+    }
+}
