2e54c0e77baedd0574b7fb624c8af0547b2302c5
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-11-17 23:12:11 +0900

    프로그래머스 해시, c++

---
+++ Programmers/CPP_Programmers/CPP_Programmers/완주하지못한선수.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    map<string, int> comp_count;
+    
+    for(auto c : completion)
+    {
+        int c_count = comp_count[c];
+        comp_count[c] = c_count + 1;
+    }
+    
+    for(auto p : participant)
+    {   
+        int c_count = comp_count[p];
+        c_count--;
+        
+        if(c_count < 0)
+        {
+            answer = p;
+            break;
+        }
+        
+        comp_count[p] = c_count;
+    }
+    
+    return answer;
+}
---
+++ Programmers/CPP_Programmers/CPP_Programmers/의상.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    unordered_map<string, int> clothes_type;
+    
+    for(int i=0; i<clothes.size(); i++)
+    {
+        if(clothes_type.count(clothes[i][1]) == 0)
+        {
+            clothes_type[clothes[i][1]] = 1;
+        }
+        else
+        {
+            clothes_type[clothes[i][1]]++;
+        }
+    }
+    
+    for(auto data : clothes_type)
+    {
+        answer *= data.second + 1;
+    }
+    
+    answer--;
+    
+    return answer;
+}
