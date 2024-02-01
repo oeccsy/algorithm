7b113e03085a1f1156772af11def451a9175bb47
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-02-02 02:40:15 +0900

    프로그래머스, 정수 삼각형, DP, c++

---
+++ Programmers/CPP_Programmers/CPP_Programmers/정수 삼각형_DP.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int d[505][505]; //d[i][j] : index i에서 triangle[i][j]를 선택했을 때 최댓값
+
+int solution(vector<vector<int>> triangle)
+{    
+    int answer = 0;
+    
+    d[0][0] = triangle[0][0];
+    
+    for(int i=1; i<triangle.size(); i++)
+    {   
+        for(int j=0; j<triangle[i].size(); j++)
+        {
+            if(j == 0)
+            {
+                d[i][j] = d[i-1][j] + triangle[i][j];
+            }
+            else
+            {
+                d[i][j] = max(d[i-1][j-1] + triangle[i][j], d[i-1][j] + triangle[i][j]);
+            }
+        }
+    }
+    
+    int max_i = triangle.size()-1;
+    int max_j = triangle[max_i].size()-1;
+    
+    for(int j=0; j<=max_j; j++)
+    {
+        if(answer < d[max_i][j]) answer = d[max_i][j];
+    }
+    
+    return answer;
+}
