47fe010bb36a21e85d3b2742311f1907ce9112be
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-02-04 17:09:43 +0900

    프로그래머스, 도둑질, DP, c++ (코드 수정)

--- Programmers/CPP_Programmers/CPP_Programmers/도둑질_DP.cpp
+++ Programmers/CPP_Programmers/CPP_Programmers/도둑질_DP.cpp
@@ -12,24 +12,19 @@ int solution(vector<int> money)
     
     // 첫 번째 집을 털지 않은 경우
     d1[0][0] = 0;
-    
-    for(int i=1; i<money.size(); i++)
-    {
-        d1[i][0] = max(d1[i-1][0], d1[i-1][1]);
-        d1[i][1] = d1[i-1][0] + money[i];
-    }
-    
-    int max_d1 = max(d1[money.size()-1][0], d1[money.size()-1][1]);
-    
     // 첫 번째 집을 턴 경우
     d2[0][1] = money[0];
     
     for(int i=1; i<money.size(); i++)
     {
+        d1[i][0] = max(d1[i-1][0], d1[i-1][1]);
+        d1[i][1] = d1[i-1][0] + money[i];
+        
         d2[i][0] = max(d2[i-1][0], d2[i-1][1]);
         d2[i][1] = d2[i-1][0] + money[i];
     }
-
+    
+    int max_d1 = max(d1[money.size()-1][0], d1[money.size()-1][1]);
     int max_d2 = d2[money.size()-1][0];
     
     answer = max(max_d1, max_d2);
