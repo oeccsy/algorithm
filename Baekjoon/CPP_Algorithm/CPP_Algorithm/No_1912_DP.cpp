7d69726f4c15039418448c1627612062e71daa85
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-02-18 23:59:56 +0900

    백준, No_1912, DP, c++

---
+++ Baekjoon/CPP_Algorithm/CPP_Algorithm/No_1912_DP.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int d[100001];
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++)
+    {
+        cin >> d[i];
+    }
+
+    int max = 0;
+
+    for(int i=1; i<=n; i++)
+    {
+        if(d[i] > max) max = d[i];
+        
+        for(int j=i+1; j<=n; j++)
+        {
+            d[i] = d[i]+d[j];
+            if(d[i] > max) max = d[i];
+        }
+    }
+
+    cout << max;
+}
