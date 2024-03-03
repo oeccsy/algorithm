010fa73c0a329e7124a227041bdbd041fa32ebe7
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-03-04 01:15:14 +0900

    백준, No_1507, 플로이드, c++ (코드 수정)

--- Algorithm/Baekjoon/C++/No_1507_플로이드.cpp
+++ Algorithm/Baekjoon/C++/No_1507_플로이드.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int n;
 int dist[21][21];
-int real_dist[21][21];
 bool adj[21][21];
 
 int main()
@@ -20,7 +19,6 @@ int main()
         for (int j = 1; j <= n; j++)
         {
             cin >> dist[i][j];
-            real_dist[i][j] = INF;
             adj[i][j] = true;
         }
     }
@@ -32,6 +30,12 @@ int main()
             for (int j = 1; j <= n; j++)
             {
                 if(i == j || j == k || k == i) continue;
+
+                if(dist[i][j] > dist[i][k] + dist[k][j])
+                {
+                    cout << -1;
+                    return 0;
+                }
                 
                 if(dist[i][j] == dist[i][k] + dist[k][j])
                 {
@@ -41,40 +45,14 @@ int main()
         }
     }
 
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=1; j<=n; j++)
-        {
-            if(adj[i][j]) real_dist[i][j] = dist[i][j];
-        }
-    }
-
-    for (int k = 1; k <= n; k++)
-    {
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {
-                if(real_dist[i][k] == INF || real_dist[k][j] == INF) continue;
-                real_dist[i][j] = min(real_dist[i][j], real_dist[i][k] + real_dist[k][j]);
-            }
-        }
-    }
-
     int sum = 0;
     for(int i=1; i<=n; i++)
     {
-        for(int j=1; j<=n; j++)
+        for(int j=i+1; j<=n; j++)
         {
-            if(dist[i][j] != real_dist[i][j])
-            {
-                cout << -1;
-                return 0;
-            }
-
             if(adj[i][j]) sum += dist[i][j];
         }
     }
 
-    cout << sum / 2;
+    cout << sum;
 }
