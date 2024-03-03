efbcaa2c825c82b2e5e2af64e28907540a5af0cc
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-03-03 22:11:49 +0900

    백준, No_1916, 다익스트라, c++

---
+++ Algorithm/Baekjoon/C++/No_1916.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+
+const int INF =  2000000000;
+
+using namespace std;
+
+struct Node;
+struct Edge;
+
+struct Node
+{
+    int num;
+    int cost;
+    vector<Edge*> adj;
+};
+
+struct Edge
+{
+    int weight;
+    Node* adj_node;
+};
+
+Node* nodes[1001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    for(int i=1; i<=n; i++)
+    {
+        nodes[i] = new Node();
+        nodes[i]->num = i;
+        nodes[i]->cost = INF;
+    }
+
+    for(int i=0; i<m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+
+        Edge* new_edge = new Edge();
+        new_edge->weight = w;
+        new_edge->adj_node = nodes[v];
+
+        nodes[u]->adj.push_back(new_edge);
+    }
+
+    int st, en;
+    cin >> st >> en;
+
+    priority_queue<pair<int, Node*>, vector<pair<int, Node*>>, greater<pair<int, Node*>>> pq;
+    nodes[st]->cost = 0;
+    pq.push({nodes[st]->cost, nodes[st]});
+
+    while(!pq.empty())
+    {
+        int min_cost = pq.top().first;
+        Node* cur_node = pq.top().second;
+        pq.pop();
+
+        if(min_cost != cur_node->cost) continue;
+        
+        for(auto edge : cur_node->adj)
+        {
+            int weight = edge->weight;
+            Node* adj_node = edge->adj_node;
+
+            if(adj_node->cost <= min_cost + weight) continue;
+
+            adj_node->cost = min_cost + weight;
+            pq.push({adj_node->cost, adj_node});
+        }
+    }
+
+    cout << nodes[en]->cost;
+}
