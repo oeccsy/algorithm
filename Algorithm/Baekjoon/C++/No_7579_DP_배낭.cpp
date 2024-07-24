e8a989e0492cbc0ee4d2eb7a997d413fe5d5f8a4
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-07-25 05:12:42 +0900

    백준, No_7579, DP, 배낭문제, c++

---
+++ Algorithm/Baekjoon/C++/No_7579_DP_배낭.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int d[105][10005]; // i번째 앱까지 고려했을 때 비용이 j인 경우 확보 가능한 메모리
+
+struct App
+{
+  int memory;
+  int cost;
+};
+
+vector<App *> apps;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  for (int i = 0; i < n; i++)
+  {
+    int memory_input;
+    cin >> memory_input;
+
+    App* new_app = new App();
+    new_app->memory = memory_input;
+
+    apps.push_back(new_app);
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    int cost_input;
+    cin >> cost_input;
+
+    apps[i]->cost = cost_input;
+  }
+
+  int ans = 10005;
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < 10005; j++)
+    {
+      if (i == 0)
+      {
+        if (j - apps[i]->cost >= 0)
+        {
+          d[i][j] = apps[i]->memory;
+        }
+      }
+      else
+      {
+        if (j - apps[i]->cost >= 0)
+        {
+          d[i][j] = max(d[i - 1][j], d[i - 1][j - apps[i]->cost] + apps[i]->memory);
+        }
+        else
+        {
+          d[i][j] = d[i - 1][j];
+        }
+      }
+
+      if (d[i][j] >= m)
+        ans = min(ans, j);
+    }
+  }
+
+  cout << ans;
+}
