a7e90a475a815bbc2be37220bf4e4562ab9b6d1b
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-09-05 22:55:36 +0900

    프로그래머스, 도넛과 막대 그래프, 그래프, c++ (코드 수정)

--- Algorithm/Programmers/C++/도넛과 막대 그래프_그래프.cpp
+++ Algorithm/Programmers/C++/도넛과 막대 그래프_그래프.cpp
@@ -1,125 +1,80 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <stack>
-#include <iostream>
 
 using namespace std;
 
 struct Node
 {
-    int node_num;
     int indegree = 0;
     int outdegree = 0;
-    int topology = -1; // 0 : donut, 1 : stick, 8 : eight
     bool is_visit = false;
     vector<Node*> adj;
-    
-    Node(int n)
-    {
-        node_num = n;
-    }
 };
 
-map<int, Node*> nodes;
+unordered_map<int, Node*> nodes;
 
 vector<int> solution(vector<vector<int>> edges)
 {
-    vector<int> answer;
+    vector<int> answer(4,0);
+    vector<Node*> start_nodes;
     
     for(auto edge : edges)
     {
         int src = edge[0];
         int dest = edge[1];
         
-        if(nodes.find(src) == nodes.end()) nodes[src] = new Node(src);
-        if(nodes.find(dest) == nodes.end()) nodes[dest] = new Node(dest);
+        if(nodes.find(src) == nodes.end()) nodes[src] = new Node();
+        if(nodes.find(dest) == nodes.end()) nodes[dest] = new Node();
         
-        nodes[src]->outdegree++;
         nodes[src]->adj.push_back(nodes[dest]);
+        nodes[src]->outdegree++;
         nodes[dest]->indegree++;
     }
     
-    int indie_node_num;
-    int donut_count = 0;
-    int stick_count = 0;
-    int eight_count = 0;
-    
-    for(auto pair : nodes)
+    for(auto& pair : nodes)
     {
         int cur_node_num = pair.first;
         Node* cur_node = pair.second;
         
-        if(cur_node->topology != -1) continue;
-        
         if(cur_node->indegree == 0 && cur_node->outdegree >= 2)
         {
-            indie_node_num = cur_node_num;
-            cur_node->is_visit = true;
-            
-            cout << "end : is indie" << endl;
-            continue;
+            answer[0] = cur_node_num;
+            start_nodes = cur_node->adj;
+            break;
         }
-        
+    }
+
+    for(auto& start_node : start_nodes)
+    {
         stack<Node*> dfs_stack;
-        cur_node->is_visit = true;
-        dfs_stack.push(cur_node);
+        start_node->is_visit = true;
+        dfs_stack.push(start_node);
         
-        vector<Node*> visit_history;
-        visit_history.push_back(cur_node);
-        
-        bool has_cycle = false;
-        bool has_center = false;
-        int topology = -1;
+        int v_count = 0;
+        int e_count = 0;
         
         while(!dfs_stack.empty())
         {
-            Node* prev_node = dfs_stack.top();
+            Node* dfs_node = dfs_stack.top();
             dfs_stack.pop();
+            v_count++;
             
-            for(auto adj_node : prev_node->adj)
-            {    
-                if(adj_node->is_visit) has_cycle = true;
-                if(adj_node->outdegree == 2) has_center = true;
-                if(adj_node->topology != -1) topology = adj_node->topology;
+            for(auto& adj_node : dfs_node->adj)
+            {
+                e_count++;
                 if(adj_node->is_visit) continue;
                 
                 adj_node->is_visit = true;
                 dfs_stack.push(adj_node);
-                
-                visit_history.push_back(adj_node);
             }
         }
         
-        if(topology == -1)
-        {
-            if(has_cycle && has_center)
-            {
-                topology = 8;
-                eight_count++;
-            }
-            else if(has_cycle)
-            {
-                topology = 0;
-                donut_count++;
-            }
-            else
-            {
-                topology = 1;
-                stick_count++;
-            }
-        }
-        
-        for(auto node : visit_history)
-        {
-            node->topology=topology;
-        }
+        if(v_count == e_count) answer[1]++;
+        if(v_count - 1 == e_count) answer[2]++;
+        if(v_count + 1 == e_count) answer[3]++;
     }
-    
-    answer.push_back(indie_node_num);
-    answer.push_back(donut_count);
-    answer.push_back(stick_count);
-    answer.push_back(eight_count);
-    
+
     return answer;
 }
