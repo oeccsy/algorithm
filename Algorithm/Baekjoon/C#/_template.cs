415e64165b8d46169863593de6847387b74e3a09
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-10-30 22:36:10 +0900

    add cs template

---
+++ Algorithm/Baekjoon/C#/_template.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Algorithm
+{
+    class Algorithm
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+        }
+    }
+}
