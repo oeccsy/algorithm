acec5192640db3d2fbe1f4bb8987054871cf668c
oeccsy <oeccsy@gmail.com>
2025-08-19 18:53:56 +0900

    백준, No_1436, 완전탐색, No_1676, 팩토리얼, 소인수

---
+++ Algorithm/Baekjoon/C++/Exhaustive Search/No_1436.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	int findCount = 0;
+	int curNum = 1;
+
+	while (findCount < n)
+	{
+		++curNum;
+
+		bool find = false;
+		string num_to_str = to_string(curNum);
+
+		int streakCount = 0;
+		for (auto& num : num_to_str)
+		{
+			if (num == '6')
+			{
+				++streakCount;
+			}
+			else
+			{
+				streakCount = 0;
+			}
+
+			if (streakCount >= 3)
+			{
+				find = true;
+				break;
+			}
+		}
+
+		if (find) ++findCount;
+	}
+
+	cout << curNum;
+}
---
+++ Algorithm/Baekjoon/C++/Math/No_1676_팩토리얼_소인수.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	// N!의 맨 뒤부터 0의 연속 개수 구하기
+
+	int n;
+	cin >> n;
+
+	int k = 0;
+	int two_count = 0;
+	int five_count = 0;
+
+	while (k < n)
+	{
+		int temp_k = ++k;
+
+		while (true)
+		{
+			if (temp_k % 2 == 0)
+			{
+				++two_count;
+				temp_k /= 2;
+			}
+			else if (temp_k % 5 == 0)
+			{
+				++five_count;
+				temp_k /= 5;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+
+	cout << ((two_count < five_count) ? two_count : five_count);
+}
