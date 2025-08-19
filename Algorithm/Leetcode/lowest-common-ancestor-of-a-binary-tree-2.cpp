2c8f48390e8ab2b961d71ad54b06c0577297c9c3
oeccsy <oeccsy@gmail.com>
2025-08-19 10:32:12 +0900

    리트코드, No_236, 이진트리에서의 LCA

---
+++ Algorithm/Leetcode/lowest-common-ancestor-of-a-binary-tree-2.cpp
@@ -0,0 +1,27 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution
+{
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if (root == nullptr) return nullptr;
+
+        TreeNode* rootFind = (root == p || root == q) ? root : nullptr;
+        if (rootFind) return root;
+
+        TreeNode* leftFind = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightFind = lowestCommonAncestor(root->right, p, q);
+        if (leftFind && rightFind) return root;
+
+        return leftFind ? leftFind : rightFind;
+    }
+};
